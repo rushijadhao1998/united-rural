9764245583345afa4dafdb504bad7b719ceebeec
Hrushikesh Jadhao <126769414+rushijadhao1998@users.noreply.github.com>
2026-03-07 20:02:38 +0530

    h

---
+++ footer.php
@@ -0,0 +1,103 @@
+<footer id="footer" class="footer light-background">
+
+<div class="container">
+
+<div class="copyright text-center">
+
+<p>
+© <span>Copyright</span>
+<strong class="px-1 sitename-footer">United Rural</strong>
+<span>All Rights Reserved</span>
+</p>
+
+</div>
+
+<div class="social-links d-flex justify-content-center">
+
+<a href="#"><i class="bi bi-twitter-x"></i></a>
+
+<a href="#"><i class="bi bi-facebook"></i></a>
+
+<a href="#"><i class="bi bi-instagram"></i></a>
+
+<a href="#"><i class="bi bi-linkedin"></i></a>
+
+</div>
+
+<div class="credits">
+Designed by
+<a href="https://rushijadhao1998.github.io/new_port/">Hrushikesh Jadhao</a>
+</div>
+
+</div>
+
+</footer>
+
+
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center">
+<i class="bi bi-arrow-up-short"></i>
+</a>
+
+<div id="preloader"></div>
+
+
+<!-- Vendor JS -->
+
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<script src="assets/vendor/aos/aos.js"></script>
+
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+
+<!-- Main JS -->
+
+<script src="assets/js/main.js"></script>
+
+
+<!-- Swiper -->
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+
+<script>
+
+const swiper = new Swiper('.swiper',{
+
+effect:"fade",
+
+fadeEffect:{
+crossFade:true
+},
+
+autoplay:{
+delay:3000,
+disableOnInteraction:false
+},
+
+speed:2000,
+loop:true,
+
+pagination:{
+el:'.swiper-pagination',
+clickable:true
+},
+
+navigation:{
+nextEl:'.swiper-button-next',
+prevEl:'.swiper-button-prev'
+},
+
+scrollbar:{
+el:'.swiper-scrollbar'
+}
+
+});
+
+</script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <link rel="shortcut icon" type="x-icon" href="assets/img/logo.png">
+
+  <title>United Rural</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Poppins&family=Raleway&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Baumans&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Main CSS -->
+  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="assets/css/header.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+  <!-- Swiper -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+</head>
+
+<body class="index-page">
+
+
+<header id="header" class="header d-flex align-items-center sticky-top">
+
+<div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+<a href="index.php" class="logo d-flex align-items-center">
+
+<img src="assets/img/logo.png" alt="">
+
+<div>
+<p class="sitename"><span class="ltr">U</span>NITED <span class="ltr">R</span>URAL</p>
+<p class="co-op">CREDIT CO-OP. SOCIETY LTD.</p>
+</div>
+
+</a>
+
+<nav id="navmenu" class="navmenu">
+
+<ul>
+
+<li><a href="index.php" class="active">Home</a></li>
+
+<li class="dropdown">
+<a href="#"><span>Services</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+
+<ul>
+<li><a href="service-deposite.php">Deposit Scheme</a></li>
+<li><a href="service-loan.php">Loan Scheme</a></li>
+<li><a href="service-pigmy.php">Pigmy Scheme</a></li>
+<li><a href="service-sms.php">SMS Banking</a></li>
+<li><a href="service-neft.php">NEFT/RTGS</a></li>
+<li><a href="service-emi.php">EMI Calculator</a></li>
+</ul>
+
+</li>
+
+<li><a href="interest.php">Interest</a></li>
+
+<li><a href="about.php">About Us</a></li>
+
+<li><a href="contact.php">Contact</a></li>
+
+<li class="dropdown">
+
+<a href="#"><span>Downloads</span>
+<i class="bi bi-chevron-down toggle-dropdown"></i></a>
+
+<ul>
+
+<li><a href="assets/forms/SHARE AC.pdf">Membership Form</a></li>
+
+<li><a href="assets/forms/fd new form.pdf">Account Opening Form</a></li>
+
+<li><a href="assets/forms/GOLD LOAN.pdf">Gold Loan Form</a></li>
+
+<li><a href="assets/forms/EM LOAN.pdf">Emergency Loan Form</a></li>
+
+<li><a href="assets/forms/Loan Form.pdf">Loan Form</a></li>
+
+<li><a href="#">Vehicle Loan Form</a></li>
+
+</ul>
+
+</li>
+
+<li>
+<a style="padding:5px 20px;" class="login_btn" href="https://banking.elioratechno.in/admin/index">LOGIN</a>
+</li>
+
+</ul>
+
+<i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+
+</nav>
+
+</div>
+
+</header>
+
+<style>
+.login_btn{
+border:2px solid #93296f;
+border-radius:7px;
+background-color:#efecf1;
+}
+</style>
